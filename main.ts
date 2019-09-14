0661e489dfc2ffc2af472caed6cc38a4a49faff1
kaise-ksrobot <arthur@kaise.com.tw>
2019-09-14 08:58:46 +0800

    v0.1.3

--- main.ts
+++ main.ts
@@ -56,10 +56,10 @@ namespace KSRobot_IOT {
 
     }
 
-    //% blockId=WIFI_SETUP
-    //% block="KSRobot WIFI Set TXD %txd| RXD %rxd| SSID %ssid| PASSWORD %passwd "
+    //% blockId=Wifi_setup
+    //% block="KSRobot WIFI Set TXD %txd| RXD %rxd| SSID %ssid| PASSWORD %passwd"
     //% weight=99
-    export function WIFI_SETUP(txd: SerialPin, rxd: SerialPin, ssid: string, passwd: string):void{
+    export function Wifi_setup(txd: SerialPin, rxd: SerialPin, ssid: string, passwd: string):void{
         
         
         IOT_SERIAL_TX = txd
@@ -69,6 +69,31 @@ namespace KSRobot_IOT {
         IOT_serial_init()
         IOT_connect_wifi()
  
+    }
+    //% blockId=ThingSpeak_set
+    //% block="ThingSpeak|Write API key = %write_api_key|Field 1 = %field1|Field 2 = %field2|Field 3 = %field3|Field 4 = %field4|Field 5 = %field5|Field 6 = %field6|Field 7 = %field7|Field 8 = %field8"
+    //% write_api_key.defl=your_write_api_key
+    export function ThingSpeak_set(write_api_key: string, field1: number, field2: number, field3: number, field4: number, field5: number, field6: number, field7: number, field8: number) {
+        if (thingspeak_connected) {
+            toSendStr = "GET /update?api_key="
+            + write_api_key
+            + "&field1=" 
+            + field1
+            + "&field2=" 
+            + field2 
+            + "&field3=" 
+            + field3 
+            + "&field4=" 
+            + field4 
+            + "&field5=" 
+            + field5 
+            + "&field6=" 
+            + field6 
+            + "&field7=" 
+            + field7 
+            + "&field8=" 
+            + field8
+        }
     }
     
 }
