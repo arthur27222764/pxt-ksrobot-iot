798fe43c6275c40af7e94ca243552b6c1ae94e61
kaise-ksrobot <arthur@kaise.com.tw>
2020-01-01 16:22:32 +0800

    v1.4.0

--- main.ts
+++ main.ts
@@ -139,10 +139,11 @@ namespace KSRobot_IOT {
         );
         serial.setRxBufferSize(128)
         serial.setTxBufferSize(128)
-        control.waitMicros(1000000)
+        control.waitMicros(500000)
         WifiDataReceived()
+        control.waitMicros(200000)
         serial.writeLine("AT+Restart=");
-        control.waitMicros(1500000)
+        control.waitMicros(1300000)
         serial.writeLine("AT+AP_SET?ssid=" + ssid + "&pwd=" + passwd + "&AP=" + ap + "=");
         for (let id_y = 0; id_y <= 4; id_y++) {
             for (let id_x = 0; id_x <= 4; id_x++) {
